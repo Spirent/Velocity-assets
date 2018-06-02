423845b26c96f94978de5a9a208d79c08db538a6
cwhitty <clark.whitty@spirent.com>
2018-06-02 10:11:13 -0500

    adding extracted data example a new feature of 7.1

---
+++ Tasks/Community/ts_scriptExamples/extractedData.py
@@ -0,0 +1,11 @@
+from datetime import datetime
+import sys, getopt
+
+
+print("[WARNING] This is a warning message")
+print("DUT Version : 10.1a")
+print("[ERROR] This is an error message")
+print("[INFO] Current time: "+ str(datetime.now()))
+print("Response Time : 34")
+print("Execution issue verified message")
+print("Finished: PASSED")
