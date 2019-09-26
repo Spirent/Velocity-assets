95e2c03b5e2941175c0de277cc9d077d7742ec04
cwhitty <clark.whitty@spirent.com>
2019-09-26 17:45:32 -0500

    updated docs

--- Tasks/Community/ts_iSiteSlackExample/isite.slack.sh
+++ Tasks/Community/ts_iSiteSlackExample/isite.slack.sh
@@ -7,7 +7,7 @@ GROUP=$(hostname)
 echo "Group Name: ${GROUP}-${INSTANCE}"
 
 # Usage for dryrun. Set dryRun = True in the python script
-sudo /usr/bin/java -jar /opt/spirent/isite/velocity-isite.jar -boot ps-production-ite.spirenteng.com:9092 -read Report -group "${GROUP}-${INSTANCE}"  | /usr/bin/jq --unbuffered -c 'select(.type == "REPORT_MESSAGE" and .reportMessage["result"] != null and (.reportMessage["reportId"] | endswith("driver") | not))' | /opt/spirent/isite/isite.py
+sudo /usr/bin/java -jar /opt/spirent/isite/velocity-isite.jar -boot vds.example.com:9092 -read Report -group "${GROUP}-${INSTANCE}"  | /usr/bin/jq --unbuffered -c 'select(.type == "REPORT_MESSAGE" and .reportMessage["result"] != null and (.reportMessage["reportId"] | endswith("driver") | not))' | /opt/spirent/isite/isite.py
 
 # Usage for in-service. Set dryRun = False in the python script
 # sudo /usr/bin/java -jar /opt/spirent/isite/velocity-isite.jar -boot ps-production-ite.spirenteng.com:9092 -read Report -group "${GROUP}-${INSTANCE}"  | /usr/bin/jq --unbuffered -c 'select(.type == "REPORT_MESSAGE" and .reportMessage["result"] != null and (.reportMessage["reportId"] | endswith("driver") | not))' | /opt/spirent/isite/isite.py > /dev/null 2>&1
